fec13f21e1dd30ed0b9d486ff1d57d44b08703f5
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-05-07 09:33:18 -0700

    Add star rating function for the restaurant

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const ListItem = (props) => (
-  <div>
-    { props.item.description }
-  </div>
-)
+const ListItem = (props) => {
+    let star = <div>★★★★★</div>
+    if (props.review.rating === '1') {
+      star = <div>★</div>
+    } else if (props.review.rating === '2') {
+      star = <div>★★</div>
+    } else if (props.review.rating === '3') {
+      star = <div>★★★</div>
+    } else if (props.review.rating === '4') {
+      star = <div>★★★★</div>
+    }
+  return (
+  
+    <div>
+      <div className="row">
+        <div className="col-3 col border-right">{ props.review.restaurant } </div>
+        <div className="col-3 col border-right">{ props.review.location }</div>
+        <div className="col-3 col border-right"> { star } </div>
+        <div className="col-sm col">{ props.review.review }</div>
+        <button onClick={() => {props.delete()}}>x</button>
+      </div>
+  </div>  
+  )
+}
 
 export default ListItem;
